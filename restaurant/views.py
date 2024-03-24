6459bd0274ff54276a3eaba67ef3f513f9c13b36
Özlem Hafalır <ozlemoztep@gmail.com>
2024-03-24 21:56:43 +0100

    add owner permission for restaurant view

--- restaurant/views.py
+++ restaurant/views.py
@@ -5,12 +5,14 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 from restaurant.models import Restaurant
 from restaurant.serializers import RestaurantSerializer
+from restaurant_booking_backend.permissions import IsOwnerOrReadOnly
 
 
 class RestaurantViewSet(viewsets.ModelViewSet):
     queryset = Restaurant.objects.annotate(popularity=Count('reservations')).all()
     serializer_class = RestaurantSerializer
     lookup_field = 'slug'
+    permission_classes = [IsOwnerOrReadOnly]
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     ordering_fields = ['name', 'created_on', 'popularity']
     filterset_fields = ['city', 'cuisines']
