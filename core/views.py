0471ba8abaaf1da10fda1f1f4dee6befab8942e9
Özlem Hafalır <ozlemoztep@gmail.com>
2024-03-25 22:29:21 +0100

    add IsAdminOrReadOnly permission for city and cuisine

--- core/views.py
+++ core/views.py
@@ -4,13 +4,16 @@ from core.models import City
 from core.models import Cuisine
 from core.serializers import CitySerializer
 from core.serializers import CuisineSerializer
+from restaurant_booking_backend.permissions import IsAdminOrReadOnly
 
 
 class CityViewSet(viewsets.ModelViewSet):
     queryset = City.objects.all()
     serializer_class = CitySerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 
 class CuisineViewSet(viewsets.ModelViewSet):
     queryset = Cuisine.objects.all()
     serializer_class = CuisineSerializer
+    permission_classes = [IsAdminOrReadOnly]
--- restaurant_booking_backend/permissions.py
+++ restaurant_booking_backend/permissions.py
@@ -8,3 +8,12 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             return True
 
         return obj.owner == request.user
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return request.user.is_staff
