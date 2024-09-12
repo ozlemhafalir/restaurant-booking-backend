69e254e8dc41713f4a2c250fdda153bf279eec3d
Özlem Hafalır <ozlemoztep@gmail.com>
2024-09-12 08:59:43 +0200

    Add restaurant image to admin panel

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from restaurant.models import Restaurant
+from restaurant.models import Restaurant, RestaurantImage
 
 admin.site.register(Restaurant)
+
+admin.site.register(RestaurantImage)
