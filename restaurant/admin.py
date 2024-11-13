851aaf7b4570de27974402db62d9e8d3e15dad91
Özlem Hafalır <ozlemoztep@gmail.com>
2024-11-13 22:27:47 +0100

    Add inline resturant image to restaurant admin

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 from restaurant.models import Restaurant, RestaurantImage
 
-admin.site.register(Restaurant)
+
+class RestaurantImageInline(admin.StackedInline):
+    model = RestaurantImage
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    search_fields = ("name", "slug", )
+    inlines = (RestaurantImageInline, )
+
 
 admin.site.register(RestaurantImage)
